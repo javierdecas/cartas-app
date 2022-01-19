e5b20528496d9f213d30a812f6cf35ae9152d3a9
javi <javier_de_castro_tsapp1ma2021@cev.com>
2022-01-19 14:26:28 +0100

    clase1

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -6,5 +6,82 @@ use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
 {
-    //
+    public function crear(Request $req)
+    {
+
+        $respuesta = ["status" => 1, "msg" => ""];
+
+        $datos = $req->getContent();
+
+        //VALIDAR EL JSON
+
+        $datos = json_decode($datos); //Se interpreta como objeto. Se puede pasar un parámetro para que en su lugar lo devuelva como array.
+
+        //VALIDAR LOS DATOS
+
+        $usuario = new Usuario();
+
+        if(isset($datos->email))
+        {
+            $usuario->nombre_usuario = $datos->nombre_usuario;
+            $usuario->email = $datos->email;
+            $usuario->password = $datos->password;
+            $usuario->rol = $datos->rol;
+        }
+        //Escribir en la base de datos
+        try
+        {
+            $usuario->save();
+            $respuesta['msg'] = "Usuario guardada con id ".$usuario->id;
+        }
+        catch(\Exception $e)
+        {
+            $respuesta['status'] = 0;
+            $respuesta['msg'] = "Se ha producido un error: ".$e->getMessage();
+        }
+
+        return response()->json($respuesta);
+    }
+    public function login(Request $req, $nombre_usuario, $password)
+    {
+
+        $respuesta = ["status" => 1, "msg" => ""];
+
+        $datos = $req->getContent();
+
+        //VALIDAR EL JSON
+
+        $datos = json_decode($datos); //Se interpreta como objeto. Se puede pasar un parámetro para que en su lugar lo devuelva como array.
+
+
+        //Buscar a la persona
+        try
+        {
+            $usuario = Usuario::find($nombre_usuario);
+
+            if($usuario)
+            {
+                if(isset($datos->password))
+                {
+                    //comprobar contraseña
+                }
+                else
+                {
+                    $respuesta['msg'] = "Introduzca una contraseña.";
+                }
+            }
+            else
+            {
+                $respuesta["msg"] = "Nombre de usuario no encontrado";
+                $respuesta["status"] = 0;
+            }
+        }
+        catch(\Exception $e)
+        {
+            $respuesta['status'] = 0;
+            $respuesta['msg'] = "Se ha producido un error: ".$e->getMessage();
+        }
+
+        return response()->json($respuesta);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('usuarios')->group(function()
+{
+    Route::put('/crear', [UsuariosController::class, 'crear']);
+    Route::post('/login', [UsuariosController::class, 'login']);
+    Route::post('/recuperarPassword', [UsuariosController::class, 'recuperarPassword']);
+});
+Route::prefix('cartas')->group(function()
+{
+    Route::put('/crear', [CartasController::class, 'crear']); //Solo admin
+});
+Route::prefix('colecciones')->group(function()
+{
+    Route::put('/crear', [ColeccionesController::class, 'crear']); //Solo admin
+});
+Route::prefix('ventas')->group(function()
+{
+    Route::put('/crear', [ColeccionesController::class, 'crear']); //Solo admin
+});
+Route::prefix('busquedas')->group(function()
+{
+    Route::get('/profesional', [ColeccionesController::class, 'crear']); //Solo profesionales y particulares
+    Route::get('/', [ColeccionesController::class, 'crear']);
+});
