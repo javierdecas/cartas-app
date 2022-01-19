999e5b1f5f37da15b266fec93658c2db7efaec06
javi <javier_de_castro_tsapp1ma2021@cev.com>
2022-01-19 12:15:40 +0100

    l

---
+++ database/migrations/2022_01_19_110142_create_ventas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('cantidad');
+            $table->float('precio');
+            $table->foreignId('cartas_id');
+            $table->foreignId('usuarios_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
